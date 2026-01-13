702006c092d1620315fb1bd8df9d6d51954311eb
Henrique <henriquevendichetis@gmail.com>
2026-01-13 15:36:50 -0300

    Mark deployment as reverted to 7dd39c1

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Updated: Reverted to version 7dd39c1
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
